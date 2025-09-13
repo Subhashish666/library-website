7bbcbf21a6fb3cf248baaeb9c7b05c0ffac00ed3
shubh <shubhcoolage@gmail.com>
2025-09-13 18:23:57 +0530

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-cci9q4oQu2g-cIhq1Cj-mtxxXL3_2M8",
+  authDomain: "library-site-b621d.firebaseapp.com",
+  projectId: "library-site-b621d",
+  storageBucket: "library-site-b621d.firebasestorage.app",
+  messagingSenderId: "448394379144",
+  appId: "1:448394379144:web:ae8f940fefa82d826cc43d",
+  measurementId: "G-3WSJ3910L8"
+};
